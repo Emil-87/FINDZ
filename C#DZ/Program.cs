a46e7d0f84da61f984cae3e22f4483b04a07fb1c
Emil-87 <emil.pegas@gmail.com>
2023-02-17 01:37:31 +0600

    first commit

---
+++ C#DZ/Program.cs
@@ -0,0 +1,22 @@
+﻿void ArrayShow(string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+    string[] ArrayFinal = {"hello", "22", "world", ":-)"};
+    string[] ArrayEmpty = new string [4];
+
+
+    for (int i=0; i<ArrayFinal.Length; i++)
+    {
+      if (ArrayFinal[i].Length <= 3)
+     {
+      ArrayEmpty[i] = ArrayFinal[i];
+      }
+    }
+
+  ArrayShow(ArrayEmpty);
